be4278ea30083c5e0d6e2a172ca2934ff3a82348
saurabhgavali23 <gavalisaurabh02@gmail.com>
2020-10-02 19:15:35 +0530

    [Saurabh] Refactor: Crate note card saperate to create note page

---
+++ src/create_note/CreateNote.js
@@ -0,0 +1,62 @@
+import React,{ useState } from 'react'
+import '../create_note/CreateNoteCss.css'
+import {
+    Button,
+    Card,
+    CardActions,
+    CardContent,
+    InputBase,
+    Typography,
+  } from "@material-ui/core";
+import ReminderIcon from "@material-ui/icons/Notifications";
+import CollaboratorIcon from "@material-ui/icons/PersonAdd";
+import ImageIcon from "@material-ui/icons/Image";
+import ColorIcon from "@material-ui/icons/ColorLens";
+import ArchiveIcon from "@material-ui/icons/Archive";
+import MoreIcon from "@material-ui/icons/MoreVert";
+
+const CreateNote = ({setShowCreateNote}) => {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+    return (
+        <div className="mainContainer">
+            <Card className="cardContainer2">
+            <CardContent className="subCardContainer2">
+              <div className="inputBaseDiv">
+                <InputBase
+                  placeholder="Titile"
+                  className="inputBase"
+                  onChange={(e) => setTitle(e.target.value)}
+                  multiline
+                />
+                <Typography>Pin</Typography>
+              </div>
+              <InputBase
+                placeholder="Take a notes"
+                className="inputBase"
+                onChange={(e) => setDescription(e.target.value)}
+                multiline
+              />
+            </CardContent>
+            <div className="actionStyle">
+              <CardActions>
+                <ReminderIcon className="iconStyle" />
+                <CollaboratorIcon className="iconStyle" />
+                <ColorIcon className="iconStyle" />
+                <ImageIcon className="iconStyle" />
+                <ArchiveIcon className="iconStyle" />
+                <MoreIcon className="iconStyle" />
+              </CardActions>
+              <CardActions onClick={()=> setShowCreateNote(false)}>
+                <Button color="primary" variant="text">
+                  close
+                </Button>
+              </CardActions>
+            </div>
+          </Card>
+          </div>
+    )
+}
+
+export default CreateNote
--- src/dash_board/DashBoard.js
+++ src/dash_board/DashBoard.js
@@ -1,26 +1,17 @@
 import {
-  Button,
   Card,
-  CardActions,
   CardContent,
-  InputBase,
   Typography,
 } from "@material-ui/core";
 import React, { useState } from "react";
 import Appbar from "../app_bar/Appbar";
-import ReminderIcon from "@material-ui/icons/Notifications";
-import CollaboratorIcon from "@material-ui/icons/PersonAdd";
 import ImageIcon from "@material-ui/icons/Image";
-import ColorIcon from "@material-ui/icons/ColorLens";
-import ArchiveIcon from "@material-ui/icons/Archive";
-import MoreIcon from "@material-ui/icons/MoreVert";
 import ViewListIcon from "@material-ui/icons/ViewList";
 import "../dash_board/DashBoardCss.css";
+import CreateNote from "../create_note/CreateNote";
 
 const DashBoard = () => {
   const [showCreateNote, setShowCreateNote] = useState(false);
-  const [title, setTitle] = useState("");
-  const [description, setDescription] = useState("");
 
   return (
     <div>
@@ -41,40 +32,7 @@ const DashBoard = () => {
           </Card>
         ) : null}
         {showCreateNote === true ? (
-          <Card className="cardContainer2">
-            <CardContent className="subCardContainer2">
-              <div className="inputBaseDiv">
-                <InputBase
-                  placeholder="Titile"
-                  className="inputBase"
-                  onChange={(e) => setTitle(e.target.value)}
-                  multiline
-                />
-                <Typography>Pin</Typography>
-              </div>
-              <InputBase
-                placeholder="Take a notes"
-                className="inputBase"
-                onChange={(e) => setDescription(e.target.value)}
-                multiline
-              />
-            </CardContent>
-            <div className="actionStyle">
-              <CardActions>
-                <ReminderIcon className="iconStyle" />
-                <CollaboratorIcon className="iconStyle" />
-                <ColorIcon className="iconStyle" />
-                <ImageIcon className="iconStyle" />
-                <ArchiveIcon className="iconStyle" />
-                <MoreIcon className="iconStyle" />
-              </CardActions>
-              <CardActions onClick={() => setShowCreateNote(!showCreateNote)}>
-                <Button color="primary" variant="text">
-                  close
-                </Button>
-              </CardActions>
-            </div>
-          </Card>
+        <CreateNote setShowCreateNote={setShowCreateNote}/>
         ) : null}
       </div>
     </div>
