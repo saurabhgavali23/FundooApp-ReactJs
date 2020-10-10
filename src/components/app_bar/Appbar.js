27c3e7af2f338f6967a309727e6bfcc6c34a9f9e
saurabhgavali23 <gavalisaurabh02@gmail.com>
2020-10-10 09:45:57 +0530

    [Saurabh] WIP: Appbar search bar

--- src/components/app_bar/Appbar.js
+++ src/components/app_bar/Appbar.js
@@ -1,4 +1,4 @@
-import { AppBar, InputBase, Typography } from "@material-ui/core";
+import { AppBar, InputBase, makeStyles, Typography } from "@material-ui/core";
 import Toolbar from "@material-ui/core/Toolbar";
 import SearchIcon from "@material-ui/icons/Search";
 import React, { useState } from "react";
@@ -6,7 +6,20 @@ import "./AppBarCss.css";
 import Profile from "../profile/Profile";
 import NoteDrawer from "../drawer/NoteDrawer";
 
+const Styles = makeStyles((theme)=>({
+  search:{
+    marginRight: theme.spacing(0),
+    marginLeft: 0,
+    width: '100%',
+    position: 'relative',
+    display: 'flex',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+}))
+
 const Appbar = () => {
+  const classes = Styles()
 const [showProfile, setShowProfile] = useState(false)
 const [openDrawer, setOpenDrawer] = useState(false)
 
@@ -14,15 +27,21 @@ const [openDrawer, setOpenDrawer] = useState(false)
     <div className="grow">
       <AppBar position="static">
         <Toolbar>
+          <div className="appBarMenuAndTitile">
           <div onClick={()=> setOpenDrawer(!openDrawer)}>
           <NoteDrawer openDrawer={openDrawer}/>
           </div>
           <Typography className="title" variant="h6" noWrap>
             FundooAap
           </Typography>
-          <div className="search">
-            <SearchIcon className="searchIcon" />
-            <InputBase placeholder="Search" className="searchInput" multiline />
+          </div>
+          <div className={classes.search}>
+            <div className="searchIcon">
+            <SearchIcon style={{ fontSize: 20}}/>
+            </div>
+            <InputBase placeholder="Search" 
+            className="searchInput" multiline 
+            />
           </div>
           <div className="profile" onClick={()=>setShowProfile(!showProfile)}>
             <Profile showProfile={showProfile}/>
