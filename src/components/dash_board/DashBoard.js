31d0ea7990e82bf07cb787afa86fabc12cb5e99e
saurabhgavali23 <gavalisaurabh02@gmail.com>
2020-10-13 08:44:15 +0530

    [Saurabh] Add: Progress bar

--- src/components/dash_board/DashBoard.js
+++ src/components/dash_board/DashBoard.js
@@ -1,41 +1,46 @@
 import {
   Card,
   CardContent,
+  CircularProgress,
   Container,
   Grid,
   Typography,
 } from "@material-ui/core";
 import React, { useEffect, useState } from "react";
 import ImageIcon from "@material-ui/icons/ImageOutlined";
-import CheckBoxOutlinedIcon from '@material-ui/icons/CheckBoxOutlined';
+import CheckBoxOutlinedIcon from "@material-ui/icons/CheckBoxOutlined";
 import "./DashBoardCss.css";
 import CreateNote from "../create_note/CreateNote";
 import { getNoteList } from "../../services/NoteServices";
 import DisplayCard from "../display_card/DisplayCard";
-import Collaborator from "../collaborator/Collaborator"
+import Collaborator from "../collaborator/Collaborator";
 import Appbar from "../app_bar/Appbar";
 
 const DashBoard = () => {
-  const [showCard, setShowCard] = useState('take_note');
+  const [showCard, setShowCard] = useState("take_note");
   const [noteList, setNoteList] = useState([]);
-  const [collabUser, setCollabUser] = useState([])
+  const [collabUser, setCollabUser] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    getNoteList().then((res) => {
-      setNoteList(res.data.data.data);
-    }).catch(err=>{
-      console.warn("error", err);
-    });
+    getNoteList()
+      .then((res) => {
+        setNoteList(res.data.data.data);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        console.warn("error", err);
+      });
   }, [showCard]);
 
   return (
     <div>
-      <Appbar/>
+      <Appbar />
       <div className="noteContainer">
-        {showCard === 'take_note' ? (
+        {showCard === "take_note" ? (
           <Card
             className="cardContainer"
-            onClick={() => setShowCard('create_note')}
+            onClick={() => setShowCard("create_note")}
           >
             <CardContent className="subCardContainer">
               <Typography className="noteTitle">Take a note...</Typography>
@@ -46,28 +51,37 @@ const DashBoard = () => {
             </CardContent>
           </Card>
         ) : null}
-        {showCard === 'create_note' ? (
+        {showCard === "create_note" ? (
           <CreateNote collabUser={collabUser} setShowCard={setShowCard} />
         ) : null}
-        {showCard === 'collaborator' ? (
-          <Collaborator setCollabUser={setCollabUser} setShowCard={setShowCard}/>
+        {showCard === "collaborator" ? (
+          <Collaborator
+            setCollabUser={setCollabUser}
+            setShowCard={setShowCard}
+          />
         ) : null}
       </div>
-      <div>
-        <Container className="displayCardContainer">
-          <Grid
-            container
-            style={{ flexGrow: 1}}
-            spacing={2}
-            direction="row"
-            alignItems="center"
-          >
-            {noteList.map((item, index) => (
-              <DisplayCard key={index} item={item}/>
-            ))}
-          </Grid>
-        </Container>
-      </div>
+      {isLoading ? (
+        <div className="progressBar">
+          <CircularProgress color="primary" />
+        </div>
+      ) : (
+        <div>
+          <Container className="displayCardContainer">
+            <Grid
+              container
+              style={{ flexGrow: 1 }}
+              spacing={2}
+              direction="row"
+              alignItems="center"
+            >
+              {noteList.map((item, index) => (
+                <DisplayCard key={index} item={item} />
+              ))}
+            </Grid>
+          </Container>
+        </div>
+      )}
     </div>
   );
 };
