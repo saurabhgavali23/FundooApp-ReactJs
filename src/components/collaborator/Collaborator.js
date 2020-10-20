0e81f68bf11000af7235f2baf56fbfee6879de51
saurabhgavali23 <gavalisaurabh02@gmail.com>
2020-10-20 09:40:09 +0530

    [Saurabh] Refactor: Collaborator page to show list of user email

--- src/components/collaborator/Collaborator.js
+++ src/components/collaborator/Collaborator.js
@@ -4,19 +4,19 @@ import {
   Card,
   CardContent,
   Divider,
-  InputBase,
+  TextField,
 } from "@material-ui/core";
 import React, { useState } from "react";
 import "./CollaboratorCss.css";
 import PersonAddIcon from "@material-ui/icons/PersonAdd";
 import { searchUserDetails } from "../../services/userServices";
 import CheckIcon from "@material-ui/icons/Check";
+import { Autocomplete } from "@material-ui/lab";
 
 const Collaborator = ({ setCollabUser, setShowCard, setIsCollabModalOpen, item }) => {
-  const [isValidEmail, setIsValidEmail] = useState(false);
   const [searchEmail, setSearchEmail] = useState("");
-  const [errorText, setErrorText] = useState("");
   const [userDetails, setUserDetails] = useState([]);
+  const [displayUserDetails, setDisplayUserDetails] = useState([])
 
   let userEmail = "unknown";
   let firstName = "unknown";
@@ -32,49 +32,45 @@ const Collaborator = ({ setCollabUser, setShowCard, setIsCollabModalOpen, item }
     });
   }
 
-  const emailValidation = () => {
-    var emailReg = /^[a-zA-Z]+[.+-]?[a-zA-Z0-9]+[@][a-zA-Z]{3,}[.][a-z]{2,4}[.]?[a-zA-Z]*[.,]?$/;
-    if (!emailReg.test(searchEmail)) {
-      setIsValidEmail(true);
-      setErrorText("Invalid Email Id");
-      return false;
-    } else {
-      setIsValidEmail(false);
-      return true;
-    }
-  };
+  const handleUserEmail = () => {
+    userDetails.map((item)=>{
+      if(item.email === searchEmail){
+        let data = []
+        data.push(item)
+        setCollabUser(data)
+      return (
+        setDisplayUserDetails([
+          ...displayUserDetails,
+          {
+            firstName: item.firstName,
+            email: item.email,
+          },
+        ])
+      );
+      }
+      return(
+        item.email
+      )
+    })
+  }
 
-  const handleUserData = (value) => {
-    if (emailValidation()) {
-      let data = {
-        searchWord: searchEmail,
-      };
-      searchUserDetails(data)
-        .then((res) => {
-          if (res.data.data.details.length !== 0) {
-            res.data.data.details.map((item) =>
-              setUserDetails([
-                ...userDetails,
-                {
-                  firstName: item.firstName,
-                  email: item.email,
-                },
-              ])
-            );
-            if(value === 'save'){
-              setCollabUser(res.data.data.details)
-              setShowCard('create_note')
-            }
-          } else {
-            setIsValidEmail(true);
-            setErrorText("user not found");
-          }
-        })
-        .catch((err) => {
-          console.warn("error", err);
-        });
+  const searchUserEmail = (value) => {
+    if(value !== ''){
+    let data = {
+      searchWord: value,
+    };
+    searchUserDetails(data)
+      .then((res) => {
+        if (res.data.data.details.length !== 0) {
+          setUserDetails(res.data.data.details)
+        }
+      })
+      .catch((err) => {
+        console.warn("error", err);
+      });
     }
-  };
+  }
+
   return (
     <div className="collabMainContainer">
       <Card className="cardMainContainer">
@@ -84,9 +80,9 @@ const Collaborator = ({ setCollabUser, setShowCard, setIsCollabModalOpen, item }
           {firstName} {lastName} (Owner)
           <div className="userEmail">{userEmail}</div>
         </CardContent>
-        {userDetails.length !== 0 && (
+        {displayUserDetails.length !== 0 && (
           <div>
-            {userDetails.map((item, index) => (
+            {displayUserDetails.map((item, index) => (
               <div key={index} className="showUserEmailWithAvatar">
                 <Avatar className="avatar">{item.firstName.slice(0, 1)}</Avatar>
                 <div>{item.email}</div>
@@ -96,22 +92,35 @@ const Collaborator = ({ setCollabUser, setShowCard, setIsCollabModalOpen, item }
         )}
         <CardContent className="inputContainer">
           <PersonAddIcon className="collabIcon" style={{ fontSize: 45 }} />
-          <InputBase
+          <div style={{width: '80%'}}>
+          <Autocomplete
+            freeSolo
+            style={{marginLeft: '5%', display: 'flex', alignItems: 'center'}}
+            options={userDetails.map((options)=> options.email)}
+            onChange={(event, value) => setSearchEmail(value)}
+            renderInput={(params)=>(
+            <TextField
+            {...params}
+            margin="normal"
+            variant="standard"
             autoFocus
             placeholder="person or email to share with"
-            className="collabInputEmail"
-            onChange={(e) => setSearchEmail(e.target.value)}
+            onChange={(e) => searchUserEmail(e.target.value)}
+            />
+            )}
           />
-          <div className="checkIconContainer" onClick={() => handleUserData()}>
-            <CheckIcon style={{ fontSize: 15 }} className="checkIcon" />
+          </div>
+          <div className="checkIconContainer">
+            <CheckIcon style={{ fontSize: 15 }} className="checkIcon" 
+              onClick={()=> handleUserEmail()}
+            />
           </div>
         </CardContent>
-        {isValidEmail && <div className="emailError">{errorText}</div>}
         <div className="collabButton">
           <Button color="primary" variant="text" onClick={() => item !== undefined ? setIsCollabModalOpen(false) : setShowCard("create_note")}>
             cancel
           </Button>
-          <div onClick={() => item !== undefined ? setIsCollabModalOpen(false) : handleUserData('save')}>
+          <div onClick={() => item !== undefined ? setIsCollabModalOpen(false) : setShowCard('create_note')}>
           <Button color="primary" variant="text">
             save
           </Button>
