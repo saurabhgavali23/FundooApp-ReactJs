b9b615ddd1826577d0237a55c6e4e7b95d5610c6
saurabhgavali23 <gavalisaurabh02@gmail.com>
2020-11-03 17:01:25 +0530

    [Saurabh] Add: AppBar in User Authorization page

--- src/pages/user_authorization/UserAuthorization.jsx
+++ src/pages/user_authorization/UserAuthorization.jsx
@@ -1,10 +1,17 @@
-import { Card, CardContent, Container, Grid } from "@material-ui/core";
+import { AppBar, Card, CardContent, Container, Grid, Toolbar, Typography } from "@material-ui/core";
 import React from "react";
 import "../user_authorization/UserAuthentication.css";
 
 const UserAuthorization = () => {
   return (
     <div>
+      <AppBar position="fixed">
+        <Toolbar>
+          <Typography variant="h4" noWrap>
+            FundooApp
+          </Typography>
+        </Toolbar>
+      </AppBar>
       <Container maxWidth="sm">
         <Grid container className="gridContainer" alignItems="center">
           <Grid item xs={12}>
@@ -16,30 +23,38 @@ const UserAuthorization = () => {
             >
               <Grid item>
                 <Card className="paper">
-                  <CardContent className="headerText">price: $99 per month</CardContent>
+                  <CardContent className="headerText">
+                    price: $99 per month
+                  </CardContent>
                   <CardContent className="serviceText">advance</CardContent>
                   <div className="bulletPointdiv">
-                    <div className="bulletPoint">{'\u2022'}</div>
+                    <div className="bulletPoint">{"\u2022"}</div>
                     <div className="priceTag">$99/month</div>
                   </div>
                   <div className="bulletPointdiv">
-                    <div className="bulletPoint">{'\u2022'}</div>
-                    <div className="priceTag">Ability to add title, 
-                    description, images, labels, checklist and colors</div>
+                    <div className="bulletPoint">{"\u2022"}</div>
+                    <div className="priceTag">
+                      Ability to add title, description, images, labels,
+                      checklist and colors
+                    </div>
                   </div>
                 </Card>
               </Grid>
               <Grid item>
                 <Card className="paper">
-                  <CardContent className="headerText">price: $49 per month</CardContent>
+                  <CardContent className="headerText">
+                    price: $49 per month
+                  </CardContent>
                   <CardContent className="serviceText">basic</CardContent>
                   <div className="bulletPointdiv">
-                    <div className="bulletPoint">{'\u2022'}</div>
+                    <div className="bulletPoint">{"\u2022"}</div>
                     <div className="priceTag">$49/month</div>
                   </div>
                   <div className="bulletPointdiv">
-                    <div className="bulletPoint">{'\u2022'}</div>
-                    <div className="priceTag">Ability to add only title and description</div>
+                    <div className="bulletPoint">{"\u2022"}</div>
+                    <div className="priceTag">
+                      Ability to add only title and description
+                    </div>
                   </div>
                 </Card>
               </Grid>
